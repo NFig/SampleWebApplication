24aeb3ecc023b0b42f0f93d039a1f7fc275a3602
Bret Copeland <bret@atlantisflight.org>
2015-02-28 20:22:24 -0500

    Initial Commit - just the default ASP.NET MVC app.

---
+++ NFig.SampleWebApplication/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NFig.SampleWebApplication
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
