5f87d83be78ce978c56441a7f15eaadd34d3900f
Bryan Ross <bryan@rossipedia.com>
2015-04-28 10:27:37 -0600

    add search box to settings page

--- NFig.SampleWebApplication/App_Start/BundleConfig.cs
+++ NFig.SampleWebApplication/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace NFig.SampleWebApplication
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                      "~/Scripts/lodash.js"));
         }
     }
 }
